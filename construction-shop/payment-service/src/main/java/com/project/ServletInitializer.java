8f2723d159c8a08480ba549409d829806818a956
Hovannisyan Armen <armog48@mail.ru>
2025-11-18 11:43:14 +0300

    add 25

--- construction-shop/payment-service/src/main/java/com/project/ServletInitializer.java
+++ construction-shop/payment-service/src/main/java/com/project/ServletInitializer.java
@@ -1,4 +1,4 @@
-package com.project.payment_service;
+package com.project;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
