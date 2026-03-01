d89f83fef2e297e3de0a34de599cf12a6ee1b2e6
Armen Hovannisyan <armog48@mail.ru>
2026-03-01 17:31:26 +0300

    194

--- construction-shop/common-lib/src/main/java/com/hardwarestore/common/dto/product/ErrorResponse.java
+++ construction-shop/common-lib/src/main/java/com/hardwarestore/common/dto/product/ErrorResponse.java
@@ -6,4 +6,20 @@ import lombok.Data;
 public class ErrorResponse {
     private String message;
     private String details;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
 }
--- construction-shop/common-lib/src/main/java/com/hardwarestore/common/dto/product/ProductDto.java
+++ construction-shop/common-lib/src/main/java/com/hardwarestore/common/dto/product/ProductDto.java
@@ -12,4 +12,60 @@ public class ProductDto {
     private Integer stock;
     private Long categoryId;
     private String categoryName;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public Integer getStock() {
+        return stock;
+    }
+
+    public void setStock(Integer stock) {
+        this.stock = stock;
+    }
+
+    public Long getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(Long categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
 }
