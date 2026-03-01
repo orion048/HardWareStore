fcd2eeff115a66ba01b00bff5ab2c64db8430180
Armen Hovannisyan <armog48@mail.ru>
2026-03-01 11:56:37 +0300

    Gateway-Service1

---
+++ construction-shop/gateway-service/src/main/java/com/project/security/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.project.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable) // отключаем CSRF
+                .authorizeExchange(exchanges -> exchanges
+                        .anyExchange().permitAll() // пока разрешаем все запросы
+                );
+        return http.build();
+    }
+}
