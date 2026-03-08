7d113c16d136ce6ed577f90119a99b317cd8371b
Hovannisyan Armen <armog48@mail.ru>
2026-03-08 16:36:29 +0300

    rfactoring user-service

--- construction-shop/user-service/src/main/java/com/project/UserServiceApplication.java
+++ construction-shop/user-service/src/main/java/com/project/UserServiceApplication.java
@@ -2,7 +2,9 @@ package com.project;
 
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
