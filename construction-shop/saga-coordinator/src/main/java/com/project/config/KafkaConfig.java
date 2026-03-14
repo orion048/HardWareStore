237dc9d667d1b2772788bb9c075ee8f4e8c4a0d5
Hovannisyan Armen <armog48@mail.ru>
2026-03-14 12:35:42 +0300

    Create Saga-Coordinator 2

--- construction-shop/saga-coordinator/src/main/java/com/project/config/KafkaConfig.java
+++ construction-shop/saga-coordinator/src/main/java/com/project/config/KafkaConfig.java
@@ -20,20 +20,6 @@ import java.util.Map;
 public class KafkaConfig {
 
     @Bean
-    public ProducerFactory<String, Object> producerFactory() {
-        Map<String, Object> config = new HashMap<>();
-        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
-        return new DefaultKafkaProducerFactory<>(config);
-    }
-
-    @Bean
-    public KafkaTemplate<String, Object> kafkaTemplate() {
-        return new KafkaTemplate<>(producerFactory());
-    }
-
-    @Bean
     public ConsumerFactory<String, Object> consumerFactory() {
         JsonDeserializer<Object> deserializer = new JsonDeserializer<>();
         deserializer.addTrustedPackages("com.hardwarestore.common.events");
