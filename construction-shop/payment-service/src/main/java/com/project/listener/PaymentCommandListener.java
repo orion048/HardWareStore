8da8f8ab2afe938976b4f56015752632d3d08f5c
Hovannisyan Armen <armog48@mail.ru>
2026-03-14 12:27:36 +0300

    Create Saga-Coorfinatir

--- construction-shop/payment-service/src/main/java/com/project/listener/PaymentCommandListener.java
+++ construction-shop/payment-service/src/main/java/com/project/listener/PaymentCommandListener.java
@@ -1,4 +1,21 @@
 package com.project.listener;
 
+import com.hardwarestore.common.events.ProcessPaymentCommand;
+import com.project.service.PaymentService;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
 public class PaymentCommandListener {
+
+    private final PaymentService paymentService;
+
+    public PaymentCommandListener(PaymentService paymentService) {
+        this.paymentService = paymentService;
+    }
+
+    @KafkaListener(topics = "saga.commands.payment", groupId = "payment-service")
+    public void onProcessPayment(ProcessPaymentCommand command) {
+        paymentService.processPayment(command);
+    }
 }
