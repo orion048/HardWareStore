6f835c1e83f84c31dbb8474dea01e9880890235a
Hovannisyan Armen <armog48@mail.ru>
2026-03-16 16:56:05 +0300

    Kafka 6

--- construction-shop/order-service/src/main/java/com/project/listener/KafkaTestListener.java
+++ construction-shop/order-service/src/main/java/com/project/listener/KafkaTestListener.java
@@ -1,5 +1,8 @@
 package com.project.listener;
 
+import com.project.event.OrderCancelledEvent;
+import com.project.event.OrderDeliveredEvent;
+import com.project.event.OrderPaidEvent;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
@@ -8,19 +11,30 @@ import org.springframework.stereotype.Component;
 @Component
 public class KafkaTestListener {
 
-    @KafkaListener(topics = "saga.events.order.delivered", groupId = "order-test")
-    public void listenDelivered(String message) {
-        log.warn("🔥 RECEIVED FROM KAFKA (DELIVERED): {}", message);
+    @KafkaListener(
+            topics = "saga.events.order.delivered",
+            groupId = "order-test",
+            containerFactory = "kafkaListenerContainerFactory"
+    )
+    public void listenDelivered(OrderDeliveredEvent event) {
+        log.warn("🔥 RECEIVED FROM KAFKA (DELIVERED): {}", event);
     }
 
-    @KafkaListener(topics = "saga.events.order.paid", groupId = "order-test")
-    public void listenPaid(String message) {
-        log.warn("🔥 RECEIVED FROM KAFKA (PAID): {}", message);
+    @KafkaListener(
+            topics = "saga.events.order.paid",
+            groupId = "order-test",
+            containerFactory = "kafkaListenerContainerFactory"
+    )
+    public void listenPaid(OrderPaidEvent event) {
+        log.warn("🔥 RECEIVED FROM KAFKA (PAID): {}", event);
     }
 
-    @KafkaListener(topics = "saga.events.order.cancelled", groupId = "order-test")
-    public void listenCancelled(String message) {
-        log.warn("🔥 RECEIVED FROM KAFKA (CANCELLED): {}", message);
+    @KafkaListener(
+            topics = "saga.events.order.cancelled",
+            groupId = "order-test",
+            containerFactory = "kafkaListenerContainerFactory"
+    )
+    public void listenCancelled(OrderCancelledEvent event) {
+        log.warn("🔥 RECEIVED FROM KAFKA (CANCELLED): {}", event);
     }
 }
-
