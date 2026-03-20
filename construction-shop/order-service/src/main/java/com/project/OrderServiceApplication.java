b0196263805384372030ec32bf1b9f2846e69f43
Armen Hovannisyan <armog48@mail.ru>
2026-03-20 12:37:06 +0300

    Order Service Fix

--- construction-shop/order-service/src/main/java/com/project/OrderServiceApplication.java
+++ construction-shop/order-service/src/main/java/com/project/OrderServiceApplication.java
@@ -2,10 +2,12 @@ package com.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients(basePackages = "com.project.client")
+@EnableDiscoveryClient
 public class OrderServiceApplication {
 
 	public static void main(String[] args) {
